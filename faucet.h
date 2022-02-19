ec278074352168063f26ff63f900c33f85315d90
truenotzero <truenotzero@gmail.com>
2022-02-20 01:28:54 +0200

    Implemented `realloc`

--- faucet.h
+++ faucet.h
@@ -1,9 +1,6 @@
 #ifndef FAUCET_H_
 #define FAUCET_H_
 
-// void *realloc(void *ptr, size_t new_size);
-// void* calloc(size_t num, size_t size);
-
 #include <stddef.h>
 
 enum faucet_memory_op {
@@ -29,13 +26,22 @@ void faucet_start(enum faucet_options);
 void faucet_stop();
 void faucet_status();
 
-// void* malloc(size_t size);
 void *faucet_p_alloc(struct faucet_trace, void *, size_t);
-// void free(void*)
 void faucet_p_free(struct faucet_trace, void *);
 
-#define malloc(size) faucet_p_alloc(faucet_trace_here(FAUCET_MALLOC), 0, size)
-#define free(ptr) faucet_p_free(faucet_trace_here(FAUCET_FREE), ptr)
+// void* malloc(size_t size);
+#define malloc(size) faucet_p_alloc(faucet_trace_here(FAUCET_MALLOC), 0, (size))
+
+// void* calloc(size_t num, size_t size);
+//#define calloc(num, size) \
+  faucet_p_alloc(faucet_trace_here(FAUCET_CALLOC), 0, (size))
+
+// void *realloc(void *ptr, size_t new_size);
+#define realloc(ptr, size)                                                     \
+  faucet_p_alloc(faucet_trace_here(FAUCET_REALLOC), (ptr), (size))
+
+// void free(void*)
+#define free(ptr) faucet_p_free(faucet_trace_here(FAUCET_FREE), (ptr))
 
 #define faucet_trace_here(oper)                                                \
   (struct faucet_trace) { .file = __FILE__, .line = __LINE__, .op = oper, }
